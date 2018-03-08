f4add58328b8fe7a9f6bc503af4fe8f5e983a180
Maneesh Thouti <33095871+Maneesh43@users.noreply.github.com>
2018-03-08 19:22:10 +0530

    Update calculator.py
    added label for displaying live time,option to exit the program and more importantly introduced many more bugs

--- calculator.py
+++ calculator.py
@@ -2,15 +2,25 @@ import tkinter
 from tkinter import *
 from tkinter import Entry
 import time
+def tick():
+        try:
+                while(True):
+                        a=time.strftime('%I:%M:%S%p')
+                        l.config(text=a,font="Helvetica 22 italic",fg="green")
+                        window.update()
+        except KeyboardInterrupt:
+                l.config(text="Done with Exception")
+                time.sleep(5)
 def calculate():
 	f=Entry.get(c)
 	g=str(f)
 	i=eval(g)
 	h.config(text=i,font="Helvetica 22 italic",fg="green")
-	window.update_idletasks()
 def clear():
 	c.delete(0,END)
 	h.config(text="")
+def quit():
+        window.destroy()
 window=tkinter.Tk()
 window.title("Calculator")
 a=tkinter.Label(text="Calculator",font="Helvetica 33 bold",fg="Blue").pack()
@@ -18,11 +28,16 @@ b=tkinter.Label(text="Enter your expression in the Entry field").pack()
 c=tkinter.Entry()
 d=tkinter.Button(text="calculate",font="Times 15 bold",fg="green",command=calculate)
 e=tkinter.Button(text="Clear",font="Times 15 bold",fg="green",command=clear)
+b2=tkinter.Button(text="quit",command=quit,font="Times 15 bold",fg="red")
 h=tkinter.Label(text="")
 c.pack()
 h.pack()
 d.pack(padx=30,pady=20,side=LEFT)
 e.pack(padx=80,pady=20,side=LEFT)
+b2.pack(padx=120,pady=20,side=LEFT)
+l1=tkinter.Label(text="")
+l1.pack()
+l=tkinter.Label(text="")
+l.pack()
+tick()
 window.mainloop()
-
-
