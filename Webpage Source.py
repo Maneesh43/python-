1c8ea7be79bb9830e96d093a16745b955fcbc6a4
Maneesh Thouti <33095871+Maneesh43@users.noreply.github.com>
2018-08-07 12:59:45 +0530

    Update Webpage Source.py

--- Webpage Source.py
+++ Webpage Source.py
@@ -1,13 +1,17 @@
 
+import threading,validators
 import tkinter
 import tkinter.scrolledtext as tstk
-from tkinter import ttk,Entry,LEFT,END
+from tkinter import ttk,Entry,LEFT,END,messagebox,StringVar
 from bs4 import BeautifulSoup
 import urllib.request as u
 def gog(_event=None):
+    global b
     #a=str(input("Enter the URL"))
     a=Entry.get(e1)
     b="http://"+a
+    if(validators.url(b)==True and  "www" and "com" in b):pass
+    else:messagebox.showinfo("enter valid url","Please enter valid url") and e1.focus()
     c=u.urlopen(b)
     d=c.read()
     #print(d)
@@ -19,18 +23,27 @@ def gog1():
     e1.delete(0,END)
     ed.delete('1.0',END)
     e1.focus()
+def gog2():
+    t1=threading.Thread(target=gog,args=[])
+    t1.start()
+#def tracer(a,b,c):
+    #gog2()
 window=tkinter.Tk()
 window.title("Get Source code of web pages")
+var=StringVar()
+#var.trace('w',tracer)
 l=tkinter.Label(text="Enter  the URL",font="Helvetica 15 italic",fg="green")
 ed=tstk.ScrolledText(font="Helvetica 15 italic",fg="blue")
-e1=tkinter.Entry()
+e1=tkinter.Entry(fg="red",font="Helvetica 12 italic",cursor="pencil")
 e1.focus()
+e1.bind('<Return>',gog)
 l.pack()
-e1.pack(padx=10,pady=10)
-b1=ttk.Button(text="Get",command=gog)
-#b1.bind('<Return>',gog)
+e1.pack(padx=10,pady=10,expand=True)
+b1=ttk.Button(text="Get",command=gog2)
+#b1.focus_set()
+b1.bind('<Return>',gog)
 b2=ttk.Button(text="clear",command=gog1)
-#b2.bind('<Return>',gog1)
+b2.bind('<Delete>',gog1)
 b1.pack(padx=10,side=LEFT,pady=10)
 b2.pack(padx=25,side=LEFT,pady=10)
 ed.pack()
