7d38bc6e09ec71daab23c6cdc0948543cc2f1e85
Maneesh Thouti <33095871+Maneesh43@users.noreply.github.com>
2018-08-07 13:10:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,16 @@
-import tkinter
-from tkinter import *
-from tkinter import Entry
+import tkinter,threading
+from tkinter import Entry,Button,StringVar,GROOVE,LEFT
 import time
 def tick():
-        try:
-                while(True):
-                        a=time.strftime('%I:%M:%S%p')
-                        l.config(text=a,font="Helvetica 22 italic",fg="green")
-                        window.update()
-        except KeyboardInterrupt:
-                l.config(text="Done with Exception")
-                time.sleep(5)
+             a=time.strftime('%I:%M:%S %p')
+             l.config(text=a,font="Helvetica 22 italic",fg="green")
+             window.after(1,tick)
+             window.update_idletasks()
 def calculate():
 	f=Entry.get(c)
 	g=str(f)
 	i=eval(g)
 	h.config(text=i,font="Helvetica 33 italic",fg="green")
-	window.after(3500,calculate)
 def clear():
 	c.delete(0,END)
 	h.config(text="")
@@ -33,6 +27,7 @@ b=tkinter.Label(text="Enter your expression in the Entry field").pack()
 c=tkinter.Entry(textvariable=var)
 d=tkinter.Button(text="calculate",font="Helvetica 15 italic",fg="green",command=calculate,relief=GROOVE,highlightcolor="green")
 e=tkinter.Button(text="Clear",font="Helvetica 15 bold",fg="green",command=clear,relief=GROOVE)
+e.focus()
 b2=tkinter.Button(text="quit",command=quit,font="Helvetica 15 bold",fg="red",relief=GROOVE)
 h=tkinter.Label(text="")
 c.pack(ipady=5)
@@ -44,6 +39,8 @@ l1=tkinter.Label(text="")
 l1.pack()
 l=tkinter.Label(text="")
 l.pack()
+t1=threading.Thread(target=tick)
+t1.start()
 #tick()
 window.mainloop()
 
